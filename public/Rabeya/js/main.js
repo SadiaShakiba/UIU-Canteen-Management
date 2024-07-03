6a37b0184cd88ebcb2a2a4b7755787f62078633d
SadiaShakibaBhuiyan <53336856+SadiaShakiba@users.noreply.github.com>
2024-07-03 16:51:54 +0600

    Add files via upload

---
+++ public/Rabeya/js/main.js
@@ -0,0 +1,114 @@
+/*  ---------------------------------------------------
+    Templater Name: Aesthetic
+    Description: Aesthetic medical template
+    Author: Colorib
+    Author URI: https://colorlib.com/
+    Version: 1.0
+    Created: Colorib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+        //Masonary
+        $('.gallery__container').masonry({
+            columnWidth: '.grid-sizer',
+            itemSelector: '.gc__item',
+            gutter: 20
+        });
+    });
+
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Canvas Menu
+    $(".canvas__open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("active");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("active");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+    /*------------------
+        Accordin Active
+    --------------------*/
+    $('.collapse').on('shown.bs.collapse', function () {
+        $(this).prev().addClass('active');
+    });
+
+    $('.collapse').on('hidden.bs.collapse', function () {
+        $(this).prev().removeClass('active');
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".header__menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true,
+        'closedSymbol': '<i class="fa fa-angle-right"></i>', // Character after collapsed parents.
+        'openedSymbol': '<i class="fa fa-angle-up"></i>', // Character after expanded parents.
+    });
+
+    /*--------------------------
+        Testimonial Slider
+    ----------------------------*/
+    $(".testimonial__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 2,
+        dots: true,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            768: {
+                items: 2,
+            },
+            0: {
+                items: 1,
+            }
+        }
+    });
+    
+    /*------------------
+		Magnific
+	--------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+    $('.image-popup').magnificPopup({
+        type: 'image'
+    });
+
+    /*-------------------
+		Nice Select
+	--------------------- */
+    $("select").niceSelect();
+
+    /*-------------------
+		Datepicker
+	--------------------- */
+    $(".datepicker").datepicker({
+        minDate: 0
+    });
+
+})(jQuery);
